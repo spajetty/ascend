5bc64b33aba343203391ff4e7118f68c6933d615
spajetty <jethrojamesaguilar.18@gmail.com>
2026-05-20 21:35:43 +0800

    relocated and renamed whip api

--- backend/emp-engagement/whip/show-whip.php
+++ backend/emp-engagement/whip/show-whip.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . '../../includes/auth-check.php';
-require_once __DIR__ . '../../vendor/autoload.php';
+require_once __DIR__ . '/../../../includes/auth-check.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../../api/');
 $dotenv->load();
 
 $host = $_ENV['DB_HOST'] ?? 'localhost';
